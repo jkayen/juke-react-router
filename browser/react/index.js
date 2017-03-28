11f9fddc1d9b9b0a208b184a2aa7a04e5aa3f287
Jacob Kayen <jacob.kayen@gmail.com>
2017-03-28 17:34:30 -0400

    setup albums redirect

--- browser/react/index.js
+++ browser/react/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRedirect } from 'react-router';
+
 import AppContainer from './containers/AppContainer';
+import Albums from './components/Albums';
 
 ReactDOM.render(
-  <AppContainer />,
+  <div>
+    <Router history={hashHistory}>
+      <Route path="/" component={AppContainer}>
+        <IndexRedirect to="/Albums" />
+        <Route path="/Albums" component={Albums} />
+      </Route>
+    </Router>
+  </div>,
   document.getElementById('app')
 );

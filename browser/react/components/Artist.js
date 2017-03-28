7005e139b7dcc320cc8f9dadd402fc80802c9e86
Jacob Kayen <jacob.kayen@gmail.com>
2017-03-28 18:24:34 -0400

    created artist and artists components

---
+++ browser/react/components/Artist.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class Artist extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  componentDidMount () {
+
+  }
+  render () {
+    return (
+
+    );
+  }
+}
